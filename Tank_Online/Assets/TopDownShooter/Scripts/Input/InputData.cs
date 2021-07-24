927e039966f481a1ffe8da4e68d55db21c2cf0e2
Berkay Özcan <berkayozcnn@outlook.com>
2021-07-24 21:53:26 +0300

    -refactor: input system

--- Tank_Online/Assets/TopDownShooter/Scripts/Input/InputData.cs
+++ Tank_Online/Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -10,5 +10,59 @@ namespace TopDownShooter.PlayerInput
         public float horizontalInput;
         public float verticalInput;
 
+        [Header("Axis Base Control")]
+        [SerializeField] private bool _axisActive;
+        [SerializeField] private string axisNameHorizontal;
+        [SerializeField] private string axisNameVertical;
+
+        [Header("Key Base Control")]
+        [SerializeField] private bool _keyBaseHorizontalActive;
+        [SerializeField] private KeyCode positiveHorizontalKeyCode;
+        [SerializeField] private KeyCode negativeHorizontalKeyCode;
+        [SerializeField] private bool _keyBaseVerticalActive;
+        [SerializeField] private KeyCode positiveVerticalKeyCode;
+        [SerializeField] private KeyCode negativeVerticalKeyCode;
+        [SerializeField] private float _increaseAmount = 0.015f;
+
+        public void ProcessInput()
+        {
+            if (_axisActive)
+            {
+                horizontalInput = Input.GetAxis(axisNameHorizontal);
+                verticalInput = Input.GetAxis(axisNameVertical);
+            }
+            else
+            {
+                if (_keyBaseHorizontalActive)
+                {
+                    KeyBaseAxisControl(ref horizontalInput, positiveHorizontalKeyCode, negativeHorizontalKeyCode);
+                }
+                if (_keyBaseVerticalActive)
+                {
+                    KeyBaseAxisControl(ref verticalInput, positiveVerticalKeyCode, negativeVerticalKeyCode);
+                }
+            }
+        }
+
+        private void KeyBaseAxisControl(ref float value, KeyCode positive, KeyCode negative)
+        {
+            bool positiveActive = Input.GetKey(positive);
+            bool negativeActive = Input.GetKey(negative);
+
+            if (positiveActive)
+            {
+                value += _increaseAmount;
+            }
+            else if (negativeActive)
+            {
+                value -= _increaseAmount;
+            }
+            else
+            {
+                value = 0;
+            }
+
+            value = Mathf.Clamp(value, -1, 1);
+        }
     }
 }
--- Tank_Online/Assets/TopDownShooter/Scripts/Input/InputManager.cs
+++ Tank_Online/Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -6,13 +6,13 @@ namespace TopDownShooter.PlayerInput
 {
     public class InputManager : MonoBehaviour
     {
-        [SerializeField] private InputData _inputData;   
+        [SerializeField] private InputData[] _inputDataArray;
         private void Update()
         {
-            _inputData.horizontalInput = Input.GetAxis("Horizontal");
-            _inputData.verticalInput = Input.GetAxis("Vertical");
+            for (int i = 0; i < _inputDataArray.Length; i++)
+            {
+                _inputDataArray[i].ProcessInput();
+            }
         }
-        
-
     }
 }
