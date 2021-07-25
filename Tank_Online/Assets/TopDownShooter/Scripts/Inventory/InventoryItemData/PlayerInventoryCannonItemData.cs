0993c5924c2e669eee7a1e88f207958abd8aafb6
Berkay Özcan <berkayozcnn@outlook.com>
2021-07-26 00:59:24 +0300

    +basic inventory processes

--- Tank_Online/Assets/TopDownShooter/Scripts/Inventory/InventoryItemData/PlayerInventoryCannonItemData.cs
+++ Tank_Online/Assets/TopDownShooter/Scripts/Inventory/InventoryItemData/PlayerInventoryCannonItemData.cs
@@ -7,6 +7,8 @@ namespace TopDownShooter.Inventory
     [CreateAssetMenu(menuName = "TopDown Shooter/Inventory/Player Inventory Cannon Item Data")]
     public class PlayerInventoryCannonItemData : AbstractPlayerInventoryItemData<PlayerInventoryCannonItemMono>
     {
+        [SerializeField] private float _damage;
+        public float Damage { get { return _damage; } }
         public override void CreateIntoInventory(PlayerInventoryController targetPlayerInventory)
         {
             var instantiated = InstantiateAndInitializePrefab(targetPlayerInventory.Parent);
